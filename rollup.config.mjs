54bda2852942223591509c433fd97b57e7daf44e
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-08-23 04:33:14 +0200

    align rollup build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,40 +1,39 @@
-import { defineConfig as Rollup } from "rollup";
-import { terser } from "rollup-plugin-terser";
-import commonjs from "@rollup/plugin-commonjs";
-import filesize from "rollup-plugin-filesize";
-import ts from "rollup-plugin-typescript2";
-import typescript from "typescript";
-import { config } from "@brixtol/rollup-utils";
+import { config, env } from '@brixtol/rollup-utils';
+import { defineConfig as Rollup } from 'rollup';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import ts from 'rollup-plugin-typescript2';
+import typescript from 'typescript';
 
-export default Rollup({
-  input: "src/index.ts",
-  output: [
-    {
-      format: "cjs",
-      file: config.output.cjs,
-      exports: "named",
-      sourcemap: process.env.prod ? false : "inline",
-    },
-    {
-      format: "esm",
-      file: config.output.esm,
-      exports: "named",
-      sourcemap: process.env.prod ? false : "inline",
-      preferConst: true,
-    },
-  ],
-  plugins: [
-    ts({
-      useTsconfigDeclarationDir: true,
-      typescript,
-    }),
-    commonjs(),
-    terser({
-      ecma: 2016,
-      compress: {
-        passes: 2,
+export default Rollup(
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        file: config.output.cjs,
+        exports: 'named',
+        sourcemap: env.is('dev', 'inline')
       },
-    }),
-    filesize(),
-  ],
-});
+      {
+        format: 'esm',
+        file: config.output.esm,
+        exports: 'named',
+        sourcemap: env.is('dev', 'inline'),
+        preferConst: true
+      }
+    ],
+    plugins: env.if('dev')(
+      [
+        ts({ useTsconfigDeclarationDir: true, typescript }),
+        commonjs()
+      ]
+    )(
+      [
+        terser({ ecma: 2016, compress: { passes: 5 } }),
+        filesize()
+      ]
+    )
+  }
+);
