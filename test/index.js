086260c7b45931919484551147344ffc136af9cb
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-08-23 04:34:08 +0200

    add test case for new mappings object export

--- test/index.js
+++ test/index.js
@@ -1,29 +1,46 @@
-import test from "ava";
-import { getCurrencySymbol } from "../package/index.es";
+import test from 'ava';
+import { getCurrencySymbol } from '../package/index';
 
-test("Currency code", (t) => {
-  t.is(getCurrencySymbol("sEK"), "kr");
-  t.is(getCurrencySymbol("eur"), "€");
-  t.is(getCurrencySymbol("USD"), "$");
-  t.is(getCurrencySymbol("AuD"), "$");
+test('Currency code symbols', (t) => {
+
+  t.is(getCurrencySymbol('sEK'), 'kr');
+  t.is(getCurrencySymbol('eur'), '€');
+  t.is(getCurrencySymbol('USD'), '$');
+  t.is(getCurrencySymbol('AuD'), '$');
 
   t.pass();
+
 });
 
-test("Currency code in uppercase", (t) => {
-  t.is(getCurrencySymbol("SEK"), "kr");
-  t.is(getCurrencySymbol("EUR"), "€");
-  t.is(getCurrencySymbol("USD"), "$");
-  t.is(getCurrencySymbol("AUD"), "$");
+test('Currency code in uppercase to symbol', (t) => {
+
+  t.is(getCurrencySymbol('SEK'), 'kr');
+  t.is(getCurrencySymbol('EUR'), '€');
+  t.is(getCurrencySymbol('USD'), '$');
+  t.is(getCurrencySymbol('AUD'), '$');
 
   t.pass();
+
 });
 
-test("Currency code in lowercase", (t) => {
-  t.is(getCurrencySymbol("sek"), "kr");
-  t.is(getCurrencySymbol("eur"), "€");
-  t.is(getCurrencySymbol("usd"), "$");
-  t.is(getCurrencySymbol("aud"), "$");
+test('Currency code in lowercase to symbol', (t) => {
+
+  t.is(getCurrencySymbol('sek'), 'kr');
+  t.is(getCurrencySymbol('eur'), '€');
+  t.is(getCurrencySymbol('usd'), '$');
+  t.is(getCurrencySymbol('aud'), '$');
 
   t.pass();
+
+});
+
+test('Exposed currency symbols export mapping object', (t) => {
+
+  t.is(getCurrencySymbol('sek'), 'kr');
+  t.is(getCurrencySymbol('eur'), '€');
+  t.is(getCurrencySymbol('usd'), '$');
+  t.is(getCurrencySymbol('aud'), '$');
+
+  t.pass();
+
 });
